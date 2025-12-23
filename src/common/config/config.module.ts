75fe5655a09c920248140c746765beb834f6f72d
Seth <zzbfor25@gmail.com>
2025-12-23 21:59:32 +0900

    Formatting

--- src/common/config/config.module.ts
+++ src/common/config/config.module.ts
@@ -3,15 +3,16 @@ import { ConfigModule as Config } from '@nestjs/config';
 import * as Joi from 'joi';
 
 const schema = Joi.object({
-  NODE_ENV: Joi.string().valid('development', 'staging', 'production').default('development'),
+  NODE_ENV: Joi.string()
+    .valid('development', 'staging', 'production')
+    .default('development'),
   PORT: Joi.number().default(3000),
   DB_HOST: Joi.string().ip().default('127.0.0.1'),
-})
+});
 const envFilePath = [`.env.${process.env.NODE_ENV || 'development'}`, '.env'];
 
-
 @Module({
-    imports: [
+  imports: [
     Config.forRoot({
       isGlobal: true,
       envFilePath,
--- src/common/filters/all-exception.filter.ts
+++ src/common/filters/all-exception.filter.ts
@@ -1,24 +1,33 @@
-import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus, Logger } from '@nestjs/common';
-import { HttpAdapterHost } from '@nestjs/core'
-import * as requestIp from 'request-ip'
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import * as requestIp from 'request-ip';
 
 @Catch()
 export class AllExceptionFilter implements ExceptionFilter {
-  private readonly logger = new Logger()
+  private readonly logger = new Logger();
   constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
 
   catch(exception: unknown, host: ArgumentsHost) {
-    const { httpAdapter } = this.httpAdapterHost
-    const ctx = host.switchToHttp()
-    const request = ctx.getRequest()
-    const response = ctx.getResponse()
-    const httpStatus = exception instanceof HttpException
-      ? exception.getStatus()
-      : HttpStatus.INTERNAL_SERVER_ERROR;
-    
-    const msg = exception instanceof HttpException
-      ? exception.getResponse()
-      : 'Internal Server Error';
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest();
+    const response = ctx.getResponse();
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const msg =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : 'Internal Server Error';
 
     const responseBody = {
       headers: request.headers,
@@ -29,9 +38,9 @@ export class AllExceptionFilter implements ExceptionFilter {
       exception: exception instanceof Error ? exception.name : 'UnknownError',
       error: msg,
       timestamp: new Date().toISOString(),
-    }
+    };
 
-    this.logger.error('[toimc]', responseBody)
-    httpAdapter.reply(response, responseBody, httpStatus)
+    this.logger.error('[toimc]', responseBody);
+    httpAdapter.reply(response, responseBody, httpStatus);
   }
 }
